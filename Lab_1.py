01417153a6660f72a42c9d807f2e533dbe113711
Владимир <vovakarpov2017@gmail.com>
2022-11-05 11:14:36 +0400

    PEP8

--- Lab_1.py
+++ Lab_1.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -7,17 +6,18 @@ from time import sleep
 
 def create_url(request):
     data = []
-    for i in range (1,2):
+    for i in range(1, 2):
         print("Parsing", i, "page")
         request.replace(' ', '20%')
         url = f'https://yandex.ru/images/search?text={request}'
         a = requests.get(url)
         sleep(1)
         soup = BeautifulSoup(a.text, "lxml")
-        tmp = soup.find_all("img", class_= "serp-item__thumb justifier__thumb")
+        tmp = soup.find_all("img", class_="serp-item__thumb justifier__thumb")
         for img in tmp: 
             tmp_url = "https:" + img.get("src")
             yield (tmp_url)
+
 
 def create_dir(src):
     temp = f'dataset/{src}'
@@ -26,22 +26,23 @@ def create_dir(src):
     if not os.path.exists(temp):
         os.mkdir(temp)
 
+
 def download_img(img_url, img_name, img_path):
     res = requests.get(img_url)
-    path = os.path.join('dataset', img_path,f'{img_name}.jpg')
-    
-    file = open (path, mode = "wb")
-    file.write (res.content)
+    path = os.path.join('dataset', img_path, f'{img_name}.jpg')
+    file = open(path, mode="wb")
+    file.write(res.content)
     file.close()
 
+
 def run(class_name):
-     create_dir(class_name)
-     count = 0
-     for i in create_url(class_name):
-        download_img(i,str(count).zfill(4), class_name)
+    create_dir(class_name)
+    count = 0
+    for i in create_url(class_name):
+        download_img(i, str(count).zfill(4), class_name)
         count += 1
         if (count % 5) == 0: 
-            print('downloaded: ' ,count)
+            print('downloaded: ', count)
         sleep(1)
 
 
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from Lab_1 import run
 
+
 if __name__ == '__main__':
     run('brown bear')
     run('polar bear')
